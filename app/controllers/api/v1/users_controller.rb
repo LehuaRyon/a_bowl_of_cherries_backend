654d36c1a00a124a759d33a136feb33ec51407cd
Jasmine Ryon <jryon12@priorypride.net>
2021-11-02 10:28:25 -0700

    adjusted user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,49 +2,50 @@ class Api::V1::UsersController < ApplicationController
     before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users/1
-  def show
-    render json: @user
-  end
+#   def show
+#     render json: @user
+#   end
 
   # POST /months
   def create
     @user = User.new(user_params)
+    render json: {user: UserSerializer.new(user), token: "token will go here"}
 
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    # if @user.save
+    #   render json: @user, status: :created, location: @user
+    # else
+    #   render json: @user.errors, status: :unprocessable_entity
+    # end
   end
 
   # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+#   def update
+#     if @user.update(user_params)
+#       render json: @user
+#     else
+#       render json: @user.errors, status: :unprocessable_entity
+#     end
+#   end
 
   # DELETE /months/1
-  def destroy
-    if @user.destroy
-      head :no_content
-    else
-      # render json: @month.errors, status: :unprocessable_entity
-      render json: {error: @user.errors.messages}, status: :unprocessable_entity
-    end
-  end
+#   def destroy
+#     if @user.destroy
+#       head :no_content
+#     else
+#       # render json: @month.errors, status: :unprocessable_entity
+#       render json: {error: @user.errors.messages}, status: :unprocessable_entity
+#     end
+#   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @@user = User.find(params[:id])
-    end
+    # def set_user
+    #   @@user = User.find(params[:id])
+    # end
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :password)
-    #   params.permit(:username, :password)
+    #   params.require(:user).permit(:username, :password)
+        params.permit(:username, :password)
     end
 end
