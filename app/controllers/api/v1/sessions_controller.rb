e6bff0b178831da04c99f418547132c64b25a3d2
Jasmine Ryon <jryon12@priorypride.net>
2021-11-02 11:20:51 -0700

    defined login within sessionsController

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::SessionsController < ApplicationController
   def login
     # byebug
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      ender json: {user: UserSerializer.new(user), token: "token will go here"}
+    else
+      render json: {error: "Incorrect username or password. Please try again."}
+    end
   end
+  # allows me to use set_user for signup AND login
 
   def autologin
   end
