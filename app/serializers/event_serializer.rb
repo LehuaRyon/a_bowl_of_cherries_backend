91ad0e7de896e8e27819a80d2f454351737678a8
Jasmine Ryon <jryon12@priorypride.net>
2021-11-02 15:15:56 -0700

    added info to serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,9 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :date, :location, :description
+  attributes :id, :name, :date, :location, :description #, :username
   has_one :month
+  has_one :user
+
+  # def username
+  #   object.user.username
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,48 +21,48 @@
 #     {name: "December"}
 # ])
 
-events = Event.create([
-    {
-        name: "Enchanted: Forest of Light",
-        date: Date.new(2021, 11, 21),
-        location: "Descanso Gardens",
-        description: "The botanical garden’s nighttime experience masterfully mixes hands-on art installations with atmospheric, luminescent forests, all against a background of uplit trees and shimmery sound effects.",
-        month_id: Month.all[10].id,
-        user_id: User.first.id
-    },
-    {
-        name: "L.A. Zoo Lights",
-        date: Date.new(2021, 11, 19),
-        location: "Los Angeles Zoo",
-        description: "The L.A. Zoo is staying open after dark every night through January. Lighting displays are inspired by the zoo’s wild residents: think elephant statues with whimsical projections, mosaic-like multicolor giraffes and twinkling butterflies.",
-        month_id: Month.all[10].id,
-        user_id: User.first.id
-    },
-    {
-        name: "The Nutcracker",
-        date: Date.new(2021, 12, 5),
-        location: "La Mirada Theatre For The Performing Arts",
-        description: "Santa Barbara's State Street Ballet stage their gloriously enjoyable production of The Nutcracker.  A story of a magical journey into the world of one young girl and her enchanted Christmas gift.",
-        month_id: Month.all[11].id,
-        user_id: User.last.id
-    },
-    {
-        name: "L.A. Auto Show",
-        date: Date.new(2021, 11, 20),
-        location: "Los Angeles Convention Center",
-        description: "See 1000 new vehicles, test drive nearly 100 models, and admire customized and exotic cars. Kids under 6 are free with paying adults.",
-        month_id: Month.all[10].id,
-        user_id: User.first.id
-    },
-    {
-        name: "Caribou",
-        date: Date.new(2021, 11, 15),
-        location: "Greek Theatre",
-        description: "Canadian producer Dan Snaith has always been interested in the intersection between electronic music and rock, finding ways to bring his synth-dappled tracks to life onstage with the help of other performers—and then taking that energy back into the studio.",
-        month_id: Month.all[10].id,
-        user_id: User.first.id
-    }
-])
+# events = Event.create([
+#     {
+#         name: "Enchanted: Forest of Light",
+#         date: Date.new(2021, 11, 21),
+#         location: "Descanso Gardens",
+#         description: "The botanical garden’s nighttime experience masterfully mixes hands-on art installations with atmospheric, luminescent forests, all against a background of uplit trees and shimmery sound effects.",
+#         month_id: Month.all[10].id,
+#         user_id: User.first.id
+#     },
+#     {
+#         name: "L.A. Zoo Lights",
+#         date: Date.new(2021, 11, 19),
+#         location: "Los Angeles Zoo",
+#         description: "The L.A. Zoo is staying open after dark every night through January. Lighting displays are inspired by the zoo’s wild residents: think elephant statues with whimsical projections, mosaic-like multicolor giraffes and twinkling butterflies.",
+#         month_id: Month.all[10].id,
+#         user_id: User.first.id
+#     },
+#     {
+#         name: "The Nutcracker",
+#         date: Date.new(2021, 12, 5),
+#         location: "La Mirada Theatre For The Performing Arts",
+#         description: "Santa Barbara's State Street Ballet stage their gloriously enjoyable production of The Nutcracker.  A story of a magical journey into the world of one young girl and her enchanted Christmas gift.",
+#         month_id: Month.all[11].id,
+#         user_id: User.last.id
+#     },
+#     {
+#         name: "L.A. Auto Show",
+#         date: Date.new(2021, 11, 20),
+#         location: "Los Angeles Convention Center",
+#         description: "See 1000 new vehicles, test drive nearly 100 models, and admire customized and exotic cars. Kids under 6 are free with paying adults.",
+#         month_id: Month.all[10].id,
+#         user_id: User.first.id
+#     },
+#     {
+#         name: "Caribou",
+#         date: Date.new(2021, 11, 15),
+#         location: "Greek Theatre",
+#         description: "Canadian producer Dan Snaith has always been interested in the intersection between electronic music and rock, finding ways to bring his synth-dappled tracks to life onstage with the help of other performers—and then taking that energy back into the studio.",
+#         month_id: Month.all[10].id,
+#         user_id: User.first.id
+#     }
+# ])
 
 # users = User.create([
 #     {
