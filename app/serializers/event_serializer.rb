2e9a9701a3e92ff165122dfaf432cec3b39fd581
Jasmine Ryon <jryon12@priorypride.net>
2021-11-02 15:33:48 -0700

    changed serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,9 +1,9 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :date, :location, :description #, :username
-  has_one :month
-  has_one :user
+  attributes :id, :name, :date, :location, :description, :username
+  # has_one :month
+  # has_one :user
 
-  # def username
-  #   object.user.username
-  # end
+  def username
+    object.user.username
+  end
 end
