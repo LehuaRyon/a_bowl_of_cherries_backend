8baa0f048dd78e88e375bcd74732b52c4600edb9
Jasmine Ryon <jryon12@priorypride.net>
2021-11-16 12:20:54 -0800

    adds user_id to event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,9 +1,13 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :date, :location, :description, :website #, :username
+  attributes :id, :name, :date, :location, :description, :website, :username, :user_id
   # has_one :month
   # has_one :user
 
-  # def username
-  #   object.user.username
-  # end
+  def username
+    object.user.username
+  end
+
+  def user_id
+    object.user.id
+  end
 end
