1ec55588ccd46c76ce82e258d77281c4c3d10227
Jasmine Ryon <jryon12@priorypride.net>
2021-11-23 16:24:09 -0800

    user can only delete his or her own events

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::EventsController < ApplicationController
   # protect_from_forgery with: :null_session
   # before_action :set_user, only: [:create, :delete]
-  before_action :set_event, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:delete]
+  # before_action :set_event, only: [:show, :update, :destroy]
 
   # GET /events
   def index
@@ -12,6 +13,7 @@ class Api::V1::EventsController < ApplicationController
 
   # GET /events/1
   def show
+    @event = Event.find(params[:id])
     render json: @event
   end
 
@@ -24,49 +26,57 @@ class Api::V1::EventsController < ApplicationController
     # else
     #   render json: @event.errors, status: :unprocessable_entity
     # end
+
+    # event = logged_in_user.events.create!(event_params)
     event = logged_in_user.events.create(event_params)
     render json: event
   end
 
   # PATCH/PUT /events/1
-  def update
-    if @event.update(event_params)
-      render json: @event
-    else
-      render json: @event.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @event.update(event_params)
+  #     render json: @event
+  #   else
+  #     render json: @event.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   def destroy
-    # event = Event.find(params[:id])
-    # user = User.find(event.user_id)
-    # event.destroy
-    # render json: user
-    # OR head :no_content
-    if @event.destroy
-      # render json: {message: "Event '#{@event.name}' successfully deleted!"}
-      head :no_content
+    event = Event.find(params[:id])
+    user = User.find(event.user_id)
+    # month = Month.find(event.month_id)
+    if user.id === logged_in_user.id
+      event.destroy
+      render json: {message: "Event '#{event.name}' successfully deleted!"}
     else
-      # render json: @month.errors, status: :unprocessable_entity
-      render json: {error: @event.errors.messages}, status: :unprocessable_entity
+      # render json: event, status: :unprocessable_entity
+      render json: {error: event.errors.messages}, status: :unprocessable_entity
     end
+
+    # if @event.destroy
+    #   # render json: {message: "Event '#{@event.name}' successfully deleted!"}
+    #   head :no_content
+    # else
+    #   # render json: @month.errors, status: :unprocessable_entity"
+    #   render json: {error: @event.errors.messages}, status: :unprocessable_entity
+    # end
   end
 
   private
-    # def set_user
-    #   @user = User.find(params[:user_id])
-    # end
-    
-    def set_event
-      @event = Event.find(params[:id])
+    def set_user
+      @user = User.find(params[:user_id])
     end
+    
+    # def set_event
+    #   @event = Event.find(params[:id])
+    # end
 
     # Only allow a list of trusted parameters through.
     # def event_params
     #   params.require(:event).permit(:name, :date, :location, :image_url, :description, :month_id)
     # end
     def event_params
-      params.permit(:name, :date, :location, :description, :website, :month_id)
-      # params.permit(:name, :date, :location, :description, :website, :month_id, :user_id)
+      # params.permit(:name, :date, :location, :description, :website, :month_id)
+      params.permit(:name, :date, :location, :description, :website, :month_id, :user_id)
     end
 end
