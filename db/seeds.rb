4692b6a56d6f20f9b7224d603d43df094015356b
Jasmine Ryon <jryon12@priorypride.net>
2021-11-26 16:42:04 -0800

    deleted readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# months = Month.create([
-#     {name: "January"},
-#     {name: "February"},
-#     {name: "March"},
-#     {name: "April"},
-#     {name: "May"},
-#     {name: "June"},
-#     {name: "July"},
-#     {name: "August"},
-#     {name: "September"},
-#     {name: "October"},
-#     {name: "November"},
-#     {name: "December"}
-# ])
+months = Month.create([
+    {name: "January"},
+    {name: "February"},
+    {name: "March"},
+    {name: "April"},
+    {name: "May"},
+    {name: "June"},
+    {name: "July"},
+    {name: "August"},
+    {name: "September"},
+    {name: "October"},
+    {name: "November"},
+    {name: "December"}
+])
 
 # events = Event.create([
 #     {
